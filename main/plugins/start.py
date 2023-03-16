9d140ae4b9332f9a4338a3673d8984cf731b227a
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-03-16 20:08:14 +0800

    Update start.py

--- main/plugins/start.py
+++ main/plugins/start.py
@@ -42,8 +42,8 @@ async def remt(event):
     except Exception:
         await event.edit("No thumbnail saved.")                        
   
-@Drone.on(events.NewMessage(incoming=True, pattern=f"{S}"))
-async def start(event):
-    text = "Send me Link of any message to clone it here, For private channel message, send invite link first.\n\n**SUPPORT:** @ZoidsSupport"
-    await start_srb(event, text)
+
+
+
+
     
